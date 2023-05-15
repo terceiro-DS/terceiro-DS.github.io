30a157dd30f9d3028843b07d116217c1ce70a79f
Pedro Henrique Vieira Ribeiro <pedrohvribeiro@gmail.com>
2023-05-15 01:16:39 -0300

    build 1.0.5

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, ViewChild, ChangeDetectorRef  } from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
@@ -14,10 +14,23 @@ export class GalleryComponent {
     return number;
   }
 
-  @ViewChild('fotos')
-  fotos!: ElementRef;
+  constructor(private cdr: ChangeDetectorRef) {}
 
-  ngAfterViewInit() {
-
+  floor(num: number): number {
+    return Math.floor(num);
   }
+
+  getImageStyle(i: number) {
+    const imgWidth = 400; // Largura máxima da imagem
+    const imgHeight = [250, 300, 350, 450, 500, 550, 600, 650, 700, 750, 800, 850]; // Alturas possíveis da imagem
+    const img = new Image();
+    img.src = `../../../assets/photos/foto${i}.jpeg`;
+    const aspectRatio = img.naturalWidth / img.naturalHeight; // Proporção da imagem
+    const height = aspectRatio >= 1 ? imgWidth / aspectRatio : imgHeight[Math.floor(Math.random() * imgHeight.length)];
+    // Definir a altura com base na proporção da imagem
+    return { 'width': imgWidth + 'px', 'height': height + 'px', 'object-fit': 'fill' };
+  }
+
+
+
 }
