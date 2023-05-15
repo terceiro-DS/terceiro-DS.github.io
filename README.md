882e033156d74ee3a808ec756523e0684ebc5b0c
Pedro Henrique Vieira Ribeiro <pedrohvribeiro@gmail.com>
2023-05-15 01:46:33 -0300

    build 1.0.5

--- README.md
+++ README.md
@@ -1,11 +1,8 @@
 # Terceiro Desenvolvimento de Sistemas
 
-[sailboatui](https://sailboatui.com/)
+[SailboatUI](https://sailboatui.com/)
 
 ```pwsh
-  ng build --output-path docs --base-href /ng-deploy/
-```
-```pwsh
-  ng build --configuration production --output-path docs --base-href /ng-deploy/
+  ng build --configuration production --output-path docs --base-href /
 ```
 Ao gerar o build, clonar o index.html e renomear como 404.html.
--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -20,16 +20,6 @@ export class GalleryComponent {
     return Math.floor(num);
   }
 
-  getImageStyle(i: number) {
-    const imgWidth = 400; // Largura máxima da imagem
-    const imgHeight = [250, 300, 350, 450, 500, 550, 600, 650, 700, 750, 800, 850]; // Alturas possíveis da imagem
-    const img = new Image();
-    img.src = `../../../assets/photos/foto${i}.jpeg`;
-    const aspectRatio = img.naturalWidth / img.naturalHeight; // Proporção da imagem
-    const height = aspectRatio >= 1 ? imgWidth / aspectRatio : imgHeight[Math.floor(Math.random() * imgHeight.length)];
-    // Definir a altura com base na proporção da imagem
-    return { 'width': imgWidth + 'px', 'height': height + 'px', 'object-fit': 'fill' };
-  }
 
 
 
