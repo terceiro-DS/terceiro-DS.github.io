ed5841c87c4a5af9d5c28d37260c54bfcaac6bb0
Pedro Henrique Vieira Ribeiro <pedrohvribeiro@gmail.com>
2023-05-17 15:23:44 -0300

    Deploy to v1.0.7

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,36 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+      '2xli': {'min': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xli': {'min': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lgi': {'min': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'mdi': {'min': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'smi': {'min': '639px'},
+      // => @media (max-width: 639px) { ... }
+    }
   },
   plugins: [],
 }
